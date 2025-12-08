9b002b1a6cab88bc4f98b52faee4f7cbc5ae99ff
Christina Adair <79288082+ChristinaAdair@users.noreply.github.com>
2025-12-08 07:24:36 -0800

    Update conf.py to ignore specific links
    Add linkcheck_ignore for bot protection links

--- conf.py
+++ conf.py
@@ -1,3 +1,8 @@
 # See the documenteer.toml for overrides of the Rubin user guide presets
 
 from documenteer.conf.guide import *  # noqa: F401, F403
+
+# Ignore links that trigger bot protection
+linkcheck_ignore = [
+    r'https://iopscience\.iop\.org/article/10\.3847/1538-3881/aafece',
+]
