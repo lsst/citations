a89bdbd59d8ebed8f6f25d5baa89ffa547a4f338
Christina Adair <79288082+ChristinaAdair@users.noreply.github.com>
2026-01-06 09:44:52 -0800

    Add linkcheck_ignore for Zenodo URLs

--- conf.py
+++ conf.py
@@ -1,3 +1,5 @@
 # See the documenteer.toml for overrides of the Rubin user guide presets
 
 from documenteer.conf.guide import *  # noqa: F401, F403
+
+linkcheck_ignore = [ r'https://zenodo\.org/.*', ]
